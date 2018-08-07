0ac45a1322e80fa11a0b1dfb77f544295d33995c
Yossaf <yossef01102015@gmail.com>
2018-08-07 09:45:26 +0200

    some fixes

--- cmd/config.js
+++ cmd/config.js
@@ -247,10 +247,16 @@ message.channel.send(embed)
 
 break;
 
+case "reset":
+
+await db.delete(`gulid__${message.guild.id}`)
+await db.delete(`cases__${guild.id}`)
 
 
 
-
+db.set(`gulid__${guild.id}`, { prefix: "//", welcome: "null", welcome_enable: false, mod_logs: "null", report_channel: "null", mod_logs_enabled: false})
+db.set(`cases__${guild.id}`, 0);
+break;
 
 
 default:
